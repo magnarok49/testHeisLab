17e5997313ef2e53bbc8e5d08744ea7d43e97e68
Pathorse <pathorse@stud.ntnu.no>
2018-01-31 19:53:04 +0100

    commit

--- source/firstFile.c
+++ source/firstFile.c
@@ -5,6 +5,7 @@ int main() {
 
     printf("Heisann, dette er en test");
     printf("Dette er mer kode");
+    printf("Magne this shit works at home @casaNardo aswell, great!");
     getchar();
     return 0;
 }
