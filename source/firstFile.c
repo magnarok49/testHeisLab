0979bd9a5d2c1a402e7d3b59ea03225e7d669904
Pathorse <pathorse@stud.ntnu.no>
2018-02-05 10:36:15 +0100

    assert truht

--- source/firstFile.c
+++ source/firstFile.c
@@ -8,6 +8,11 @@ int main() {
     printf("Magne this shit works at home @casaNardo aswell, great!");
     printf("yello");
     printf("jalalaaj");
+    if (1>0)
+    {
+        return 1;
+    }
+
     getchar();
     return 0;
 }
