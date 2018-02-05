4dac5323bcad9bbabac5f24f8fc1b96a8af115d8
Pathorse <pathorse@stud.ntnu.no>
2018-02-05 10:20:53 +0100

    greet mars as well

--- source/firstFile.c
+++ source/firstFile.c
@@ -6,6 +6,7 @@ int main() {
     printf("Heisann, dette er en test");
     printf("Dette er mer kode");
     printf("Magne this shit works at home @casaNardo aswell, great!");
+    printf("yello");
     getchar();
     return 0;
 }
