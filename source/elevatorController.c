40d31bec6d0d43f615a4481319c06cc00a2c2761
Pathorse <pathorse@stud.ntnu.no>
2018-02-21 14:07:57 +0100

    floorhthkst0ko

--- source/elevatorController.c
+++ source/elevatorController.c
@@ -122,7 +122,8 @@ void stopElevator(int floor)
         elev_set_button_lamp(0,floor,0);
         elev_set_button_lamp(1,floor,0);
         printf("Arrived at floor ");
-        printf(char(floor+48));
+        char c = floor+48;
+        printf("%c",c);
         printf(" successfully");
     }
 }
